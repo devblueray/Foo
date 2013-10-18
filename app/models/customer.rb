041b7070b0ea38e81c34bfa511e6bcf2c4597836
Jason Lane <ror@jasonlane.me>
2013-10-18 12:58:40 -0400

    Added info fields to customer

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -4,4 +4,15 @@ class Customer < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 	has_many :galleries
+
+	rails_admin do
+		field :email
+		field :password
+		field :firstName
+		field :lastName
+		field :birthday
+		field :physAddress
+		field :phone
+		field :phone2
+	end
 end
