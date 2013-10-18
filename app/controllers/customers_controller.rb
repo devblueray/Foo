82f8810b73b4f2291baa48c4151f03e825e1feec
Jason Lane <ror@jasonlane.me>
2013-10-17 23:23:09 -0400

    Added Welcome page and checks for login

--- app/controllers/customers_controller.rb
+++ app/controllers/customers_controller.rb
@@ -1,7 +1,11 @@
 class CustomersController < ApplicationController
-	before_filter :authenticate_customer!
+	before_filter :authenticate_customer!, :except => [:show]
+
 	def index
-	
+	end
+
+	def show
+		redirect_to root_path
 	end
 
 end
---
+++ app/controllers/welcome_controller.rb
@@ -0,0 +1,5 @@
+class WelcomeController < ApplicationController
+	before_filter :authenticate_user!, :except => [:index, :show]
+	def index
+  end
+end
