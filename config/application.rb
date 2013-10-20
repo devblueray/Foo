7bc8080aed2d4dde0a3d834fdf3e82053bb99304
Jason Lane <ror@jasonlane.me>
2013-10-20 11:00:14 -0400

    Converted to mysql

--- config/application.rb
+++ config/application.rb
@@ -8,8 +8,8 @@ Bundler.require(:default, Rails.env)
 
 module Foo
   class Application < Rails::Application
-		config.assets.enabled = true
-		config.serve_static_assets = true
+		#config.assets.enabled = true
+		#config.serve_static_assets = false
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
