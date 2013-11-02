67163bd03f21a36713d2a67c5da07b26e34bd01b
Jason Lane <ror@jasonlane.me>
2013-11-02 00:54:08 -0400

    Added header for site

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,8 @@
 class WelcomeController < ApplicationController
-	before_filter :authenticate_user!, :except => [:index, :show]
+	#before_filter :authenticate_user!, :except => [:index, :show]
+	 before_filter :authenticate_customer! 
 	def index
+	  @galleries = Gallery.where(customer_id: current_customer.id)
+	  
   end
 end
